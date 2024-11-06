e1bff40c6d2941ac0eb9fe88356fcb82c5637e3e
Jonas Hotzan <jhotzan@uni-bremen.de>
2024-11-06 09:12:34 +0100

    zwischenpush

--- ueb01_alternative/DS02/src/Main.java
+++ ueb01_alternative/DS02/src/Main.java
@@ -1,33 +1,7 @@
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        Counter counter = new Counter();
 
-
-        Thread incrementThread1 = new Thread() {
-            public void run(){
-                for (int i = 0; i < 1000; i++) {
-                    counter.increment();
-                }
-            }
-        };
-
-        Thread incrementThread2 = new Thread(){
-            public void run(){
-                for (int i = 0; i < 1000; i++) {
-                    counter.decrement();
-                }
-            }
-        };
-
-        incrementThread1.start();
-        incrementThread2.start();
-
-        incrementThread1.join();
-        incrementThread2.join();
-
-
-        System.out.println(counter.value());
 
     }
 

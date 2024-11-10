3551c8df1aef3ae6da5bf173c53e2911a05af384
aleksandraB2002 <alexandraf2002@outlook.de>
2024-11-10 13:23:01 +0100

    Post ist jetzt auch da

--- ueb2/Ueb2/src/Client.java
+++ ueb2/Ueb2/src/Client.java
@@ -8,6 +8,7 @@ public class Client {
         Client c =  new Client();
         //String s = c.httpGet("GET / HTTP/1.1\r\n", "stud.fh-wedel.de", 80);
         c.httpGet("GET / HTTP/1.1\r\n", "localhost", 8080);
+        // c.httpPost("POST / HTTP/1.1\r\n", "localhost", 8080, "Data");
     }
 
     public void httpGet(String msg, String host, int port) throws IOException {
@@ -32,5 +33,26 @@ public class Client {
 
     }
 
+    public void httpPost(String msg, String host, int port, String data) throws IOException {
+        Socket s = new Socket(host, port);
+
+        PrintWriter out = new PrintWriter(s.getOutputStream());
+        out.print("POST / HTTP/1.1\r\n");
+        out.print("Host: " + host + "\r\n");
+        out.print("Content-Length: " + data.length() + "\r\n");
+        out.print("Content-Type: text/plain\r\n");
+        out.print("\r\n");
+        out.print(data);
+        out.flush();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        in.close();
+        out.close();
+        s.close();
+    }
 
 }
--- ueb2/Ueb2/src/ClientHandlingInstance.java
+++ ueb2/Ueb2/src/ClientHandlingInstance.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.Socket;
 
-public class ClientHandlingInstance implements Runnable{
+public class ClientHandlingInstance implements Runnable {
     Socket socket;
 
     public ClientHandlingInstance(Socket pSocket) throws IOException {
@@ -17,7 +17,7 @@ public class ClientHandlingInstance implements Runnable{
             String req = in.readLine();
             System.out.println("Server got the following request: " + req);
 
-            if(req != null && req.contains("GET")) {
+            if (req != null && req.contains("GET")) {
                 get(in, out, req);
             } else if (req != null && req.contains("POST")) {
                 post(in, out, req);
@@ -44,38 +44,33 @@ public class ClientHandlingInstance implements Runnable{
     }
 
     private void post(BufferedReader in, BufferedWriter out, String req) throws IOException {
-        String[] tokens = req.split(" ");
-        String filePath = tokens[1].equals("/") ? "/index.html" : tokens[1];
-        File file = new File(filePath);
-
-        String line;
         int contentLength = 0;
-
-        // Read headers until empty line (HTTP headers are separated from body by empty line)
+        String line;
+         // recieves content
         while ((line = in.readLine()) != null && !line.isEmpty()) {
-            if(line.startsWith("Content-Length:")) {
-                contentLength = Integer.parseInt(line.split(" ")[1]);
+            if (line.startsWith("Content-Length:")) {
+                contentLength = Integer.parseInt(line.split(": ")[1]);  // Ensure correct parsing
             }
             System.out.println("Header: " + line);
         }
 
-        // Only read body if we have content length
         if (contentLength > 0) {
             char[] bodyContent = new char[contentLength];
             in.read(bodyContent, 0, contentLength);
-            String postDat = new String(bodyContent);
-            System.out.println("Daten: " + postDat);
-            File data = new File("data.txt");
-            writeFile("data.txt", postDat);
-            sendResponse(out, 200, "OK", file);
-            System.out.println("File has been created at " + filePath);
+            String postData = new String(bodyContent);
+            System.out.println("Received POST data: " + postData);
+
+            // Write the received data to a file
+            writeFile("data.txt", postData);
+            sendResponse(out, 200, "OK", null); // Send a 200 OK response with a success message
         } else {
             sendResponse(out, 400, "Bad Request - No Content Length", null);
         }
     }
 
+
     private void writeFile(String fileName, String content) {
-        try{
+        try {
             BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
             out.write(content);
             out.close();
@@ -85,24 +80,36 @@ public class ClientHandlingInstance implements Runnable{
     }
 
     private void sendResponse(BufferedWriter out, int code, String msg, File file) throws IOException {
-        out.write("HTTP/1.1" + " " + code + " " + msg + "\r\n");
+        out.write("HTTP/1.1 " + code + " " + msg + "\r\n");
         out.write("Content-Type: text/html\r\n");
         out.write("\r\n");
 
-        if(file != null) {
-            try {
-                BufferedReader in = new BufferedReader(new FileReader(file));
+        if (file != null) {
+            try (BufferedReader in = new BufferedReader(new FileReader(file))) {
                 String line;
-                while((line = in.readLine()) != null) {
+                while ((line = in.readLine()) != null) {
                     out.write(line + "\r\n");
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                sendErrorResponse(out, 500, "Internal Server Error");
             }
         } else {
-            out.write("Error" + " " + code + " " + msg + "\r\n");
+            // if the file equals null, we can send a success message for POST requests
+            if (code == 200) {
+                out.write("<html><body><h1>POST data received successfully!</h1></body></html>");
+            } else {
+                sendErrorResponse(out, 404, "File Not Found");
+            }
         }
 
+        out.flush();
+    }
+
+    private void sendErrorResponse(BufferedWriter out, int code, String msg) throws IOException {
+        out.write("HTTP/1.1 " + code + " " + msg + "\r\n");
+        out.write("Content-Type: text/html\r\n");
+        out.write("\r\n");
+        out.write("<html><body><h1>" + msg + "</h1></body></html>");
         out.flush();
     }
 }
